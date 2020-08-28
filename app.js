8e83885f51171432b3cb9d0458f136794a04ece1
Artois <artoislabs@gmail.com>
2020-08-28 13:05:46 +0200

    first

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-let add = document.querySelector('.add-note');
+const add = document.querySelector('.add-note');
+const deleteNoteElt = document.querySelector('.delete-note');
 
-add.addEventListener('click', function(){
-    let champs = document.querySelector('input[name="note"]').value; 
-    alert(champs)
-    document.prepend('ul li').innerHTML = champs;
-})
+add.addEventListener('click', addNote)
+deleteNoteElt.addEventListener('click', deleteNote.bind(this))
+
+function addNote()
+{
+    const noteValue = document.querySelector('input[name="note"]').value; 
+    
+
+    const newLi = document.createElement('li');
+    const liContent = document.createTextNode(noteValue)
+    const btnDelNote = document.createElement('button')
+    btnDelNote.innerHTML = 'supprimer'
+    btnDelNote.classList.add('delete-note')
+    newLi.appendChild(liContent) 
+    newLi.appendChild(btnDelNote)
+    document.querySelector('.elements').appendChild( newLi );
+}
+
+function deleteNote(elt)
+{
+    elt.remove()
+}
